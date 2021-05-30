baa04b70bbd232dcabac40635beec823f4239439
Matt Turco <matt.turco@gmail.com>
2021-05-29 19:04:44 -0500

    Reading list explainer wording

--- src/routes/reading-list/index.svelte
+++ src/routes/reading-list/index.svelte
@@ -44,8 +44,8 @@
 	<header>
 		<h1>Reading List</h1>
 		<p>
-			Weekly-ish list of articles, blog posts, tweets, talks, etc that I found interesting and/or
-			learned something from. Usually related to web development, software, or design in some way
+			Weekly-ish list of articles, blog posts, tweets, talks, projects, etc that I found interesting
+			or learned something from. Usually related to web development, software, or design in some way
 			but occasionally general interest. Not all content is necessarily fresh—I share things as I
 			find them!
 		</p>
