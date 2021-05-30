3f69188e504b6939e4f754d6f25790080d97a1e9
Matt Turco <matt.turco@gmail.com>
2021-05-30 13:03:38 -0500

    Fix error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
+	import { dev } from '$app/env';
+
 	export let status: number;
 	export let error: Error;
+
+	const offline = typeof navigator !== 'undefined' && navigator.onLine === false;
+	const title = offline ? 'Offline' : status;
+	const message = offline ? 'Find the internet and try again' : error?.message;
 </script>
 
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
 <main>
-	<h1>{status}</h1>
-	<p>{error.message}</p>
+	<h1>{title}</h1>
+	<p>{message}</p>
 </main>
+
+{#if dev && error?.stack}
+	<pre>{error.stack}</pre>
+{/if}
 
 <style>
 	main {
