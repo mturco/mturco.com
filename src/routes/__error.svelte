7ddba8e26cfd516de180278feea0be54d0b39dca
Matt Turco <matt.turco@gmail.com>
2022-06-05 12:17:32 -0600

    Fix error template

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,21 +1,28 @@
+<script lang="ts" context="module">
+  import type { Load } from '@sveltejs/kit';
+
+  export const load: Load = ({ error, status }) => ({
+    props: { status, error },
+  });
+</script>
+
 <script lang="ts">
   import { dev } from '$app/env';
 
   export let status: number;
   export let error: Error;
-
-  const offline = typeof navigator !== 'undefined' && navigator.onLine === false;
-  const title = offline ? 'Offline' : status;
-  const message = offline ? 'Find the internet and try again' : error?.message;
 </script>
 
 <svelte:head>
-  <title>{title}</title>
+  <title>{status}</title>
 </svelte:head>
 
 <main>
-  <h1>{title}</h1>
-  <p>{message}</p>
+  <h1>{status}</h1>
+
+  {#if error?.message}
+    <p>{error.message}</p>
+  {/if}
 </main>
 
 {#if dev && error?.stack}
@@ -29,5 +36,24 @@
     flex-direction: column;
     text-align: center;
     justify-content: center;
+    align-items: center;
+  }
+
+  /* prettier-ignore */
+  h1 {
+    --color: var(--color-primary);
+    --width: 1px;
+    font-size: 5rem;
+    margin: 1rem 0;
+    color: var(--color-background);
+    text-shadow:
+      calc(var(--width) * -1) calc(var(--width) * -1) 0 var(--color),
+      0 calc(var(--width) * -1) 0 var(--color),
+      var(--width) calc(var(--width) * -1) 0 var(--color),
+      var(--width) 0 0 var(--color),
+      var(--width) var(--width) 0 var(--color),
+      0 var(--width) 0 var(--color),
+      calc(var(--width) * -1) var(--width) 0 var(--color),
+      calc(var(--width) * -1) 0 0 var(--color);
   }
 </style>
